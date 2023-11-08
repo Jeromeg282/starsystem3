3328fd92128c98e8eb805506d74b93e84214de8b
Jeromeg282 <113056411+Jeromeg282@users.noreply.github.com>
2023-11-08 13:28:35 -0800

    a

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #Python3
+
+import random
+#need to restore from db, then retrive then display
+filename = "die.csv"
+class stargeneration():
+    mainworld=0
+    atmosphere=0
+    populus=0
+    gov=0
+    techlevel=0
+
+
+
+    def __init__(self):
+        self.data=open(filename,"r")
+        self.mainworld=(random.randint(1,6) + random.randint(1,6) - 2)
+        self.atmosphere=(random.randint(1,6))
