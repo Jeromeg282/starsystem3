54814cb5091d74e5b19e30cab27745537b7e06c7
Jerome282 <113056411+Jeromeg282@users.noreply.github.com>
2024-01-29 20:08:15 -0800

    .

--- main.py
+++ main.py
@@ -1,59 +1,7 @@
 #Python3
-
+import sqlite3
 import random
-#need to restore from db, then retrive then display
-filename = "die.csv"
-class stargeneration():
-    mainworld=0
-    atmosphere=0
-    populus=0
-    hydros=0
-    gov=0
-    techlevel=0
-    lawlevel=0
-
-    def __init__(self):
-        self.data=open(filename,"r")
-        self.mainworld=(random.randint(1,6) + random.randint(1,6) - 2)
-        self.atmosphere=(random.randint(1,6) + random.randint(1.6) - 7 + self.mainworld)
-        if self.mainworld == 0:self.atmosphere = 0
-        self.hydros = (random.randint(1,6) + random.randint(1,6) - 7 + self.mainworld)
-        self.populus=(random.randint(1,6) + random.randint(1,6) - 2)
-        self.gov=(random.randint(1,6) + random.randint(1,6) - 7 + self.populus)
-        self.lawlevel=(random.randint(1,6) + random.randint(1,6) - 7 + self.gov)
-
-
-        self.info = {"dice roll": self.mainworld}
-        self
-
-
-
-
-#def generateClusters():
-    #c = 0
-    #cx = 0
-    #cy = 0
-    #cz = 0
-    #rad = random.uniform(CLUSRADA, CLUSRADB)
-    #num = random.uniform(NUMCLUSA, NUMCLUSB)
-    #clusters.append((cx, cy, cz, rad, num))
-    #c = 1
-    #while c < NUMCB:
-        # random distance from centre
-        #dist = random.uniform(CLUSRAD, GALX)
-        # any rotation- clusters can be anywhere
-        #theta = random.random() * 360
-        #cx = math.cos(theta * math.pi / 180.0) * dist
-        #cy = math.sin(theta * math.pi / 180.0) * dist
-        #cz = random.random() * GALZ * 2.0 - GALZ
-        #rad = random.uniform(CLUSRADA, CLUSRADB)
-        #num = random.uniform(NUMCLUSA, NUMCLUSB)
-        # add cluster to clusters array
-        #clusters.append((cx, cy, cz, rad, num))
-        # process next
-        #c = c+1
-        #sran = 0
-        #cran = 0
+import json
 
 
 
@@ -62,16 +10,73 @@ class stargeneration():
 
 
 
+class Star():
+    def __init__(self) -> None:
+        file = 'database.db'
+        self.connection = sqlite3.connect(file)
+        self.cursor = self.connection.cursor()
+        query = """
+        CREATE TABLE IF NOT EXISTS starsystem (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            planetName TINYTEXT,
+            starport INT,
+            navelbase BOOLEAN,
+            gasgiant TINYTEXT,
+            planetoid INT,
+            scoutbase TINYTEXT,
+            size INT,
+            atm INT,
+            hyd INT,
+            population INT,
+            govt INT,
+            lawlvl INT,
+            techlvl INT
+        );
+        """
+        self.cursor.execute(query)
+    
+
+    def starportType(self):
+        roll = sum(self.dice(2))
+
+        if roll in [2, 3, 4]:
+            self.starport = 10
+        elif roll in [5, 6]:
+            self.starport = 11
+        elif roll in [7, 8]:
+            self.starport = 12
+        elif roll == 9:
+            self.starport = 13
+        elif roll in [10, 11]:
+            self.starport = 14
+        elif roll == 12:
+            self.starport = 16
+
+        return self.starport
 
 
 
+    def isNavelBase(self):
+        roll = sum(self.dice(2))
+        self.navelbase = roll > 7
+        return self.navelbase
+        
 
-
-
-
-
-
-
+    def isGasGiant(self):
+        roll = sum(self.dice(2))
+        self.Gasgiant = roll > 9
+        return self.Gasgiant
+    
+    
+    def isPlanetoids(self):
+        roll = sum(self.dice(2))
+        self.planetoids = roll > 6
+        return self.planetoids
+        
+    def isScoutBase(self):
+        roll = sum(self.dice(2))
+        self.scoutbase = roll > 6
+        return self.scoutbase
 
 
 
