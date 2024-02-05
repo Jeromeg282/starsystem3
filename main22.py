a508f51a1d3aeeaa290fb7a860c44b8446ab43ef
keital354 <113056447+Keita354@users.noreply.github.com>
2024-02-05 02:42:31 -0800

    GG

--- main22.py
+++ main22.py
@@ -1,6 +1,7 @@
 import sqlite3
 import random
 import names
+from prettytable import PrettyTable
 
 
 class Star:
@@ -27,6 +28,14 @@ class Star:
         );
         """
         self.cursor.execute(query)
+        self.planet_upp()
+        query = "select * from starsystem"
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print("Num, Name, StarportLevel, NavelBase, GasGiant, ScoutBase, Size, Atm, Hyd, Population, Government, LawLevel, TechLevel\n")
+        for i in result:
+            print(i)
+        
 
     def starport_type(self):
         roll = sum(self.dice(2))
@@ -161,39 +170,69 @@ class Star:
 
 class UniverseGeneration:
     def __init__(self):
-        self.file = 'stardatabase.db'
+        self.file = 'database.db'
         self.connection = sqlite3.connect(self.file)
         self.cursor = self.connection.cursor()
         self.search_query()
 
     def search_query(self):
         print("""
-            The options to search for are: 
-            \033[38;5;221m(1)\033[0m planet Name 
-            \033[38;5;221m(2)\033[0m size
-            \033[38;5;221m(3)\033[0m atmosphere 
-            \033[38;5;221m(4)\033[0m hydrographics 
-            \033[38;5;221m(5)\033[0m population 
-            \033[38;5;221m(6)\033[0m government 
-            \033[38;5;221m(7)\033[0m law level 
-            \033[38;5;221m(8)\033[0m tech level  
-            \033[38;5;221m(9)\033[0m Fetch all  
-            \033[38;5;221m(10)\033[0m Generate Star
+            \n\n***************************Welcome to the Star Systems Console*****************************  
+            \nThe menu options are: \n
+            \033[38;5;221mOption[1]\033[0m <---Search By Planet Name---> 
+            \033[38;5;221mOption[2]\033[0m <---Search By Planet Size--->
+            \033[38;5;221mOption[3]\033[0m <---Search By Planet Atmosphere---> 
+            \033[38;5;221mOption[4]\033[0m <---Search By Planet Hydrographics---> 
+            \033[38;5;221mOption[5]\033[0m <---Search By Planet Population---> 
+            \033[38;5;221mOption[6]\033[0m <---Search By Planet Government---> 
+            \033[38;5;221mOption[7]\033[0m <---Search By Planet Law level---> 
+            \033[38;5;221mOption[8]\033[0m <---Search By Planet Technical level--->  
+            \033[38;5;221mOption[9]\033[0m <---Print All Planet Info--->  
+            \033[38;5;221mOption[10]\033[0m <---Generate Star System--->              
+            \n********************************************************************************************  
+              
         """)
         try:
-            self.selection = int(input("Enter your search query: "))
+            self.selection = int(input("Please enter your menu option here: \n\n"))
         except ValueError:
             self.search_query()
 
-        self.symbol = input("Enter the symbol you wish to use for the search (e.g., >, <, =) ")
-        if 1 <= self.selection <= 8:
-            self.search_by_category()
+        self.symbol = input("Enter the operator symbol for planet search (e.g., >, <, =), or just hit Enter key to bypass: ")
+
+        if self.selection == 1:            
+            return(self.search_name())
+    
+        elif self.selection == 2:            
+            return(self.search_size())
+        
+        elif self.selection == 3:    
+            return(self.search_atm())
+        
+        elif self.selection == 4:           
+            return(self.search_hyd())
+        
+        elif self.selection == 5:
+            return(self.search_pop())
+        
+        elif self.selection == 6:
+            return(self.search_gov())
+        
+        elif self.selection == 7:           
+            return(self.search_law())
+        
+        elif self.selection == 8:    
+            return(self.search_tech())
+        
         elif self.selection == 9:
-            self.get_all()
+            return(self.get_all())
+        
         elif self.selection == 10:
             self.generate_star()
+            #self.search_query()
+        
         else:
             self.search_query()
+
 
     def search_by_category(self):
         search_functions = [
@@ -204,10 +243,10 @@ class UniverseGeneration:
         search_functions[self.selection - 1]()
 
     def display_result(self, result):
+        t = PrettyTable(['Planet Name', 'Star Port', 'Naval Base', 'Gas Giant', 'Planetoid', 'Scout Base', 'Planet Size', 'Atmosphere', 'Hydrographics', 'Population', 'Government', 'Law Level', 'Technical Level'])
         for i in result:
-            txt = f"                                                                                  Planet Name: {i[1]}\n------------------------------\nStarPort: {i[2]}       Naval Base: {i[3]}\nGasGiant: {i[4]}     Planetoid:{i[5]}\nScout Base: {i[6]}  Planet Size: {i[7]}\nAtmosphere: {i[8]}      Hydrogenics: {i[9]}\nPopulation: {i[10]}      Government: {i[11]}\nLaw level: {i[12]}       Tech Level: {i[13]}\n"
-            new_str = txt.center(500)
-            print(new_str)
+            t.add_row([{i[1]}, {i[2]}, {i[3]}, {i[4]}, {i[5]}, {i[6]}, {i[7]}, {i[8]}, {i[9]}, {i[10]}, {i[11]}, {i[12]}, {i[13]}])
+        print(t)
 
     def execute_query(self, query):
         self.cursor.execute(query)
@@ -216,14 +255,14 @@ class UniverseGeneration:
         self.search_query()
 
     def search_name(self):
-        name = input("Enter the name of the planet you wish to search for: ")
+        name = input("Enter the name of the planet to search for: ")
         query = f"SELECT * FROM starsystem WHERE planetName {self.symbol} '{name}'"
         self.execute_query(query)
 
     def search_size(self):
         size = 0
         try:
-            size = int(input("Enter the planet size(1-6) you want to search for: "))
+            size = int(input("Enter the planet size(1-6) to search for: "))
         except ValueError:
             self.search_size()
         query = f"SELECT * FROM starsystem WHERE size {self.symbol} {size}"
@@ -232,7 +271,7 @@ class UniverseGeneration:
     def search_atm(self):
         atm = 0
         try:
-            atm = int(input("Enter the planet atmosphere(1-6) you want to search for: "))
+            atm = int(input("Enter the planet atmosphere(1-6) to search for: "))
         except ValueError:
             self.search_atm()
         query = f"SELECT * FROM starsystem WHERE atm {self.symbol} {atm}"
@@ -241,7 +280,7 @@ class UniverseGeneration:
     def search_hyd(self):
         hyd = 0
         try:
-            hyd = int(input("Enter the planet hydrogeneics(1-6) you want to search for: "))
+            hyd = int(input("Enter the planet hydrogeneics(1-6) to search for: "))
         except ValueError:
             self.search_hyd()
         query = f"SELECT * FROM starsystem WHERE hyd {self.symbol} {hyd}"
@@ -250,7 +289,7 @@ class UniverseGeneration:
     def search_pop(self):
         pop = 0
         try:
-            pop = int(input("Enter the planet population(1-6) you want to search for: "))
+            pop = int(input("Enter the planet population(1-6) to search for: "))
         except ValueError:
             self.search_pop()
         query = f"SELECT * FROM starsystem WHERE population {self.symbol} {pop}"
@@ -259,7 +298,7 @@ class UniverseGeneration:
     def search_gov(self):
         govt = 0
         try:
-            govt = int(input("Enter the planet government(1-6) you want to search for: "))
+            govt = int(input("Enter the planet government(1-6) to search for: "))
         except ValueError:
             self.search_gov()
         query = f"SELECT * FROM starsystem WHERE govt {self.symbol} {govt}"
@@ -268,7 +307,7 @@ class UniverseGeneration:
     def search_law(self):
         law = 0
         try:
-            law = int(input("Enter the planet law level(1-6) you want to search for: "))
+            law = int(input("Enter the planet law level(1-6) to search for: "))
         except ValueError:
             self.search_law()
         query = f"SELECT * FROM starsystem WHERE lawlvl {self.symbol} {law}"
@@ -277,7 +316,7 @@ class UniverseGeneration:
     def search_tech(self):
         tech = 0
         try:
-            tech = int(input("Enter the planet tech level(1-6) you want to search for: "))
+            tech = int(input("Enter the planet tech level(1-6) to search for: "))
         except ValueError:
             self.search_tech()
         query = f"SELECT * FROM starsystem WHERE techlvl {self.symbol} {tech}"
